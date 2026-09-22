7a7ca79c62767e9cdab56e637b71d994089a04f5
Alhadis <gardnerjohng@gmail.com>
2026-09-22 10:44:25 +1000

    Apply Alhadis/.emacs.d@09cc16c to lone Elisp file

--- snippets/fundamental-mode/.yas-setup.el
+++ snippets/fundamental-mode/.yas-setup.el
@@ -1,7 +1,6 @@
-;;
+;; -*- lexical-binding: t; -*-
+
 ;; Workaround for snippets not being inherited from `fundamental-mode'.
 ;; See: https://github.com/joaotavora/yasnippet/issues/557#issuecomment-72368826
-;;
 (add-hook 'yas-minor-mode-hook
-          (lambda()
-            (yas-activate-extra-mode 'fundamental-mode)))
+  (lambda() (yas-activate-extra-mode 'fundamental-mode)))
